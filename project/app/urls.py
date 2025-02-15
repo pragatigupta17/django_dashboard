7fc8eaec04cabd66d186a02fe1e412099efe7fc9
Pragati Gupta <pragatigupta0104@gmail.com>
2025-02-15 14:51:04 +0530

    h

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-      path('',views.home,name='home'),
+    path('',views.signin,name='signin'),
+    path('home/',views.home,name='home'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
     path('registration/',views.registration,name='registration'),
