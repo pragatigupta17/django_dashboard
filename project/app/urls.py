2acccba53fbb43cf20f034c44e8df7da846182b7
Pragati Gupta <pragatigupta0104@gmail.com>
2025-02-15 20:25:12 +0530

    p

--- project/app/urls.py
+++ project/app/urls.py
@@ -3,6 +3,7 @@ from app import views
 
 urlpatterns = [
     path('',views.signin,name='signin'),
+    path('admindashboard/',views.admindashboard,name='admindashboard'),
     path('home/',views.home,name='home'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
--- project/app/views.py
+++ project/app/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 def signin(request):
     return render(request, 'signin.html')
+    print (request.method)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if username == 'admin' and password == 'admin':
+            return render(request,'admindashboard.html',{})
+        else:
+                username:User.object.filter(stu_name=name)
+def admindashboard(request):
+    return render(request, 'admindashboard.html')
+
 def home(request):
     return render(request,'home.html')
 
